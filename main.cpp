78762dab60bb27222ddfe7b758edf87710c95102
Emma <mtyselmeci@gmail.com>
2024-05-08 22:20:21 +0200

    Panels work now, onto the 'widgets'

--- main.cpp
+++ main.cpp
@@ -3,23 +3,17 @@
 #include "helpers.hpp"
 #include "frame.hpp"
 #include "testLabel.hpp"
+#include "panel.hpp"
 
 
-struct MyStruct {
-    int i;
-    MyStruct(int initialValue) : i(writeI(this,initialValue)) {}
-    int writeI(MyStruct* m, int initialValue) {
-        return initialValue + 10;
-    }
-};
 
 int main() {
+    Rect r1(100,0,100,100);
+    Rect r2(0,100,200,200);
+
     Frame f({1000,600});
-    Rect r(100,100,200,200);
+    Panel* p = new Panel(Layer::TOP,r2,&f);
+    new TestLabel(Layer::TOP,r1,p); //Using implicit conversion or whatever this is called
 
-    MyStruct m();
-
-    std::cout << "Main method memory adress " << &r << "\n";
-    new TestLabel(Layer::TOP,r,&f); //Using implicit conversion or whatever this is called
     f.start();
 }
--- panel.cpp
+++ panel.cpp
@@ -1,10 +1,13 @@
+#include <iostream>
 #include "panel.hpp"
 
+#define ws << " " <<
+
 void Panel::getCoordDelta(Rect& other_dim) const {
+    std::cout << dim.p1.x ws dim.p1.y ws dim.p2.x ws dim.p2.y << "\n";
+    std::cout << other_dim.p1.x ws other_dim.p1.y ws other_dim.p2.x ws other_dim.p2.y << "\n";
     other_dim.p1.x += dim.p1.x;
     other_dim.p1.y += dim.p1.y;
-    other_dim.p2.x += dim.p2.x;
-    other_dim.p2.y += dim.p2.y;
 }
 
 bool Panel::check(event& ev) {}
